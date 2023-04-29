4d535c7340728cfdc9726e75ee0cb38d20b6bb24
Sasha Petrov <petrov.evpanet@gmail.com>
2023-04-29 09:53:02 +0100

    Added seo texts Removed unused components Added README.md

--- README.md
+++ README.md
@@ -1,35 +1,23 @@
-# writelikenative
+# Write Like a Native
 
-This template should help get you started developing with Vue 3 in Vite.
+Write Like a Native is a Vue.js web app that helps you improve your English text by using OpenAI's GPT-3. The app offers three types of prompts to choose from:
 
-## Recommended IDE Setup
+- **Blog**: Improves the text in a way that makes it more suitable for a blog post.
+- **Message**: Adjusts the text to be more conversational, ideal for messaging with friends or colleagues.
+- **Correct**: Corrects grammar and punctuation errors in the text.
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+## How it works
 
-## Customize configuration
+1. Enter your text in the input text area.
+2. Choose one of the three prompt options: Blog, Message, or Correct.
+3. Click the "Improve" button.
+4. The app will use OpenAI's GPT-3 to improve your text based on the chosen prompt.
+5. The improved text will be displayed in the output text area.
 
-See [Vite Configuration Reference](https://vitejs.dev/config/).
+## Requirements
 
-## Project Setup
+To use Write Like a Native, you need to have an OpenAI API key. If you don't have one, follow the instructions in the app to get your API key. Once you have the key, enter it in the app, and you're ready to start improving your text.
 
-```sh
-npm install
-```
+## Credits
 
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
+Write Like a Native is developed by [Sasha Petrov](https://petrov.substack.com/), ~95% of code written by ChatGPT (GPT-4 model).
--- src/App.vue
+++ src/App.vue
@@ -247,6 +247,19 @@
       <p class="text-sm text-slate-500 mt-4">
         Simply choose a prompt, enter your text, and let the AI work its magic to elevate your writing.
       </p>
+      <a @click="showMore = !showMore" class="block mt-4 mb-4 text-blue-600 hover:cursor-pointer">
+        {{ showMore ? "Learn less" : "Learn more" }}
+      </a>
+      <div v-if="showMore" class="text-slate-500 text-sm">
+        <p class="mb-6">If you're looking to improve your writing skills and create high-quality content, AI-powered writing assistants can be game-changers. From enhancing blog posts and articles to crafting clear and concise messages, artificial intelligence offers a wide range of tools to elevate your writing. In this article, we'll discuss how AI-based writing aids can help you achieve better communication, whether for personal or professional purposes.</p>
+        <h3 class="text-xl font-semibold mb-2">Artificial Intelligence for Clear and Concise Messages</h3>
+        <p class="mb-6">AI-driven message clarity helpers, like "Write like a native," can assist you in crafting natural and conversational messages. These tools focus on email drafting assistance, improving grammar and spelling, and ensuring your written communication is simple yet effective. By acting as an editor, spelling corrector, and grammar improver, AI writing aids help you sound like a native speaker in any messaging app or email.</p>
+        <h3 class="text-xl font-semibold mb-2">AI-Powered Blog Post and Article Enhancement</h3>
+        <p class="mb-6">Intelligent blog post editors and AI-based content optimization tools, such as "Write like a native," are designed to improve the quality and readability of your content on platforms like Substack or Medium. These AI-powered writing assistants work by refining your text paragraph by paragraph, ensuring your content is engaging, informative, and free of pretentious language.</p>
+        <h3 class="text-xl font-semibold mb-2">Error-Free Text Generation with AI</h3>
+        <p class="mb-6">AI-assisted proofreading tools and punctuation and style checkers, like "Write like a native," focus on correcting grammar, spelling, and punctuation to produce polished, error-free text. By simply providing a word, sentence, or paragraph, you'll receive the improved text without any additional explanations. This advanced writing support can be especially beneficial for non-native speakers aiming to improve their writing quality.</p>
+        <p>In conclusion, AI-driven writing tools offer a wide range of assistance to elevate your written communication skills. From AI-powered writing assistants for blog posts and articles to tools that improve message clarity and error-free texts, the possibilities for better writing are endless. Embrace the power of AI-based writing aids and experience the difference in your writing today.</p>
+      </div>
     </div>
 
   </div>
@@ -273,6 +286,7 @@ export default {
       loading: false,
       copied: false,
       showDetails: false,
+      showMore: false,
     };
   },
   created() {
