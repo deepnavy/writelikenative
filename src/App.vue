600f874e898406d83772a12cf775b51dbd6e9ecd
Sasha Petrov <petrov.evpanet@gmail.com>
2023-04-30 13:06:06 +0100

    Added APIKeyModal component Added AppDescription component

--- src/App.vue
+++ src/App.vue
@@ -23,106 +23,11 @@
       </button>
     </header>
     <div class="z-50 container mx-auto px-4">
-      <div
+      <APIKeyModal
         v-if="showModal"
-        class="fixed z-50 inset-0 overflow-y-auto"
-        aria-labelledby="modal-title"
-        role="dialog"
-        aria-modal="true"
-      >
-        <div
-          class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-        >
-          <div
-            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-            aria-hidden="true"
-            @click="showModal = false"
-          ></div>
-          <span
-            class="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
-          <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full sm:p-6">
-            
-          <div class="text-left">
-            <h3
-              class="text-lg leading-6 font-medium text-gray-900"
-              id="modal-title"
-            >
-              Enter your OpenAI API Key
-            </h3>
-            <div class="mt-2">
-              <input
-                v-model="userAPIKey"
-                type="text"
-                class="border w-full p-2 mt-2 rounded-md"
-                placeholder="Enter your API key here"
-              />
-            </div>
-          </div>
-
-
-          <p class="mt-2 text-gray-600 text-sm">
-            Follow these steps to retrieve OpenAI API key:
-            <a
-              href="#"
-              @click.prevent="showDetails = !showDetails"
-              class="text-blue-600"
-            >
-              {{ showDetails ? "Hide details" : "Show details" }}
-            </a>
-          </p>
-
-          <div v-if="showDetails" class="mt-2 text-sm text-gray-600">
-            <ol class="list-decimal list-outside pl-6">
-              <li>
-                Go to the OpenAI website: 
-                <a
-                  href="https://www.openai.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  class="text-blue-600"
-                >
-                  https://www.openai.com/
-                </a>
-              </li>
-              <li>Click on "Get Started" or "Sign in" if you already have an account.</li>
-              <li>Create an account or sign in to your existing account.</li>
-              <li>After signing in, you'll be directed to the OpenAI Dashboard.</li>
-              <li>Navigate to the API section by clicking "API" in the left sidebar menu or by visiting: https://platform.openai.com/signup</li>
-              <li>Follow the instructions to access or sign up for the API. If you're eligible, you'll be provided with an API key.</li>
-              <li>The API key should look like a long alphanumeric string (e.g., "sk-12345abcdeABCDEfghijKLMNOP").</li>
-            </ol>
-          </div>
-
-            
-          <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-            <button
-              @click="saveAPIKey"
-              type="button"
-              class="w-full inline-flex justify-center rounded-md border border-transparent px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Save
-            </button>
-            <button
-              @click="deleteAPIKey"
-              class="sm:ml-3 bg-red-600 text-white mt-3 w-full inline-flex justify-center rounded-md border px-4 py-2  text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
-            >
-              Delete
-            </button>
-            <button
-              @click="showModal = false"
-              type="button"
-              class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm"
-            >
-              Cancel
-            </button>
-          </div>
-          </div>
-        </div>
-      </div>
+        @close="showModal = false"
+        @clear-output-text="clearOutputText"
+      />
 
       <div class="w-full mt-14">
         <div class="text-center mb-4">
@@ -228,39 +133,8 @@
         
       </div>
     </div>
-    <div id="app-description" class="bg-slate-100 px-4 py-3 mt-14 mx-4 rounded-lg sm:max-w-md sm:mx-auto">
-      <h2 class="text-lg font-semibold text-slate-600 mb-4">How does this app work?</h2>
-      <p class="text-sm text-slate-500">
-        This app uses AI to help you improve your writing. It offers three options to cater to different cases:
-      </p>
-      <ul class="list-disc list-outside pl-6 mt-4 text-sm text-slate-500">
-        <li class="pb-2">
-          <span class="font-semibold">Blog:</span> Designed to enhance your blog posts or articles, making them more engaging and informative. Improving the text paragraph by paragraph is recommended.
-        </li>
-        <li class="pb-2">
-          <span class="font-semibold">Message:</span> Perfect for crafting clear and concise messages, emails, or other forms of written communication.
-        </li>
-        <li class="pb-2">
-          <span class="font-semibold">Just correct:</span> Focused on refining grammar, spelling, and punctuation for polished and error-free text.
-        </li>
-      </ul>
-      <p class="text-sm text-slate-500 mt-4">
-        Simply choose a prompt, enter your text, and let the AI work its magic to elevate your writing.
-      </p>
-      <a @click="showMore = !showMore" class="block mt-4 mb-4 text-blue-600 hover:cursor-pointer">
-        {{ showMore ? "Learn less" : "Learn more" }}
-      </a>
-      <div v-if="showMore" class="text-slate-500 text-sm">
-        <p class="mb-6">If you're looking to improve your writing skills and create high-quality content, AI-powered writing assistants can be game-changers. From enhancing blog posts and articles to crafting clear and concise messages, artificial intelligence offers a wide range of tools to elevate your writing. In this article, we'll discuss how AI-based writing aids can help you achieve better communication, whether for personal or professional purposes.</p>
-        <h3 class="text-xl font-semibold mb-2">Artificial Intelligence for Clear and Concise Messages</h3>
-        <p class="mb-6">AI-driven message clarity helpers, like "Write like a native," can assist you in crafting natural and conversational messages. These tools focus on email drafting assistance, improving grammar and spelling, and ensuring your written communication is simple yet effective. By acting as an editor, spelling corrector, and grammar improver, AI writing aids help you sound like a native speaker in any messaging app or email.</p>
-        <h3 class="text-xl font-semibold mb-2">AI-Powered Blog Post and Article Enhancement</h3>
-        <p class="mb-6">Intelligent blog post editors and AI-based content optimization tools, such as "Write like a native," are designed to improve the quality and readability of your content on platforms like Substack or Medium. These AI-powered writing assistants work by refining your text paragraph by paragraph, ensuring your content is engaging, informative, and free of pretentious language.</p>
-        <h3 class="text-xl font-semibold mb-2">Error-Free Text Generation with AI</h3>
-        <p class="mb-6">AI-assisted proofreading tools and punctuation and style checkers, like "Write like a native," focus on correcting grammar, spelling, and punctuation to produce polished, error-free text. By simply providing a word, sentence, or paragraph, you'll receive the improved text without any additional explanations. This advanced writing support can be especially beneficial for non-native speakers aiming to improve their writing quality.</p>
-        <p>In conclusion, AI-driven writing tools offer a wide range of assistance to elevate your written communication skills. From AI-powered writing assistants for blog posts and articles to tools that improve message clarity and error-free texts, the possibilities for better writing are endless. Embrace the power of AI-based writing aids and experience the difference in your writing today.</p>
-      </div>
-    </div>
+    <AppDescription />
+   
 
   </div>
   <footer class="text-center text-sm text-gray-500 mt-8 mb-8">
@@ -272,20 +146,23 @@
 
 <script>
 import createOpenaiInstance from "@/openai";
-import { ref, computed } from "vue";
 import { useStore } from "vuex";
+import APIKeyModal from "./components/APIKeyModal.vue";
+import AppDescription from "./components/AppDescription.vue";
 
 export default {
+  components: {
+    APIKeyModal,
+    AppDescription
+  },
   data() {
     return {
       showModal: false,
-      userAPIKey: '',
       inputText: "",
       outputText: "",
       selectedPrompt: "blog",
       loading: false,
       copied: false,
-      showDetails: false,
       showMore: false,
     };
   },
@@ -294,7 +171,6 @@ export default {
     if (!store.getters.getAPIKey) {
       this.outputText = "Please provide (top-right corner) an OpenAI API key to use the app.";
     }
-    this.userAPIKey = store.getters.getAPIKey;
   },
   computed: {
     isInputEmpty() {
@@ -400,12 +276,8 @@ export default {
         });
       }
     },
-    saveAPIKey() {
-      if (this.userAPIKey) {
-        this.$store.commit('setAPIKey', this.userAPIKey);
-        this.showModal = false;
-        this.outputText = "";
-      }
+    clearOutputText(){
+      this.outputText = "";
     },
     copyOutputText() {
       if (this.outputText) {
@@ -419,12 +291,6 @@ export default {
           }
         );
       }
-    },
-    deleteAPIKey() {
-      sessionStorage.removeItem('openaiApiKey');
-      this.$store.commit('setAPIKey', "");
-      this.userAPIKey = "";
-      this.showModal = false;
     },
   },
 };
---
+++ src/components/AppDescription.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="app-description" class="bg-slate-100 px-4 py-3 mt-14 mx-4 rounded-lg sm:max-w-md sm:mx-auto">
+      <h2 class="text-lg font-semibold text-slate-600 mb-4">How does this app work?</h2>
+      <p class="text-sm text-slate-500">
+        This app uses AI to help you improve your writing. It offers three options to cater to different cases:
+      </p>
+      <ul class="list-disc list-outside pl-6 mt-4 text-sm text-slate-500">
+        <li class="pb-2">
+          <span class="font-semibold">Blog:</span> Designed to enhance your blog posts or articles, making them more engaging and informative. Improving the text paragraph by paragraph is recommended.
+        </li>
+        <li class="pb-2">
+          <span class="font-semibold">Message:</span> Perfect for crafting clear and concise messages, emails, or other forms of written communication.
+        </li>
+        <li class="pb-2">
+          <span class="font-semibold">Just correct:</span> Focused on refining grammar, spelling, and punctuation for polished and error-free text.
+        </li>
+      </ul>
+      <p class="text-sm text-slate-500 mt-4">
+        Simply choose a prompt, enter your text, and let the AI work its magic to elevate your writing.
+      </p>
+      <a @click="showMore = !showMore" class="block mt-4 mb-4 text-blue-600 hover:cursor-pointer">
+        {{ showMore ? "Learn less" : "Learn more" }}
+      </a>
+      <div v-if="showMore" class="text-slate-500 text-sm">
+        <p class="mb-6">If you're looking to improve your writing skills and create high-quality content, AI-powered writing assistants can be game-changers. From enhancing blog posts and articles to crafting clear and concise messages, artificial intelligence offers a wide range of tools to elevate your writing. We'll discuss how AI-based writing aids can help you achieve better communication, whether for personal or professional purposes.</p>
+        <h3 class="text-xl font-semibold mb-2">Artificial Intelligence for Clear and Concise Messages</h3>
+        <p class="mb-6">AI-driven message clarity helpers, like "Write like a native," can assist you in crafting natural and conversational messages. These tools focus on email drafting assistance, improving grammar and spelling, and ensuring your written communication is simple yet effective. By acting as an editor, spelling corrector, and grammar improver, AI writing aids help you sound like a native speaker in any messaging app or email.</p>
+        <h3 class="text-xl font-semibold mb-2">AI-Powered Blog Post and Article Enhancement</h3>
+        <p class="mb-6">Intelligent blog post editors and AI-based content optimization tools, such as "Write like a native," are designed to improve the quality and readability of your content on platforms like Substack or Medium. These AI-powered writing assistants work by refining your text paragraph by paragraph, ensuring your content is engaging, informative, and free of pretentious language.</p>
+        <h3 class="text-xl font-semibold mb-2">Error-Free Text Generation with AI</h3>
+        <p class="mb-6">AI-assisted proofreading tools and punctuation and style checkers, like "Write like a native," focus on correcting grammar, spelling, and punctuation to produce polished, error-free text. By simply providing a word, sentence, or paragraph, you'll receive the improved text without any additional explanations. This advanced writing support can be especially beneficial for non-native speakers aiming to improve their writing quality.</p>
+        <p>In conclusion, AI-driven writing tools offer a wide range of assistance to elevate your written communication skills. From AI-powered writing assistants for blog posts and articles to tools that improve message clarity and error-free texts, the possibilities for better writing are endless. Embrace the power of AI-based writing aids and experience the difference in your writing today.</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      showMore: false,
+    };
+  },
+};
+</script>
