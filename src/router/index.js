10b764b1f02a5a3b4654c7e57bfaf2e048985387
Sasha Petrov <petrov.evpanet@gmail.com>
2024-01-10 20:55:38 +0000

    Fix base url

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '../App.vue'
 
-const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? import.meta.env.VITE_FLASK_APP_BASE_URL_LOCAL
-    : import.meta.env.VITE_FLASK_APP_BASE_URL_PROD;
-    
-
 // Define routes
 const routes = [
   {
@@ -18,7 +12,7 @@ const routes = [
 
 // Create router instance
 const router = createRouter({
-  history: createWebHistory(baseUrl),
+  history: createWebHistory('/'),
   routes
 })
 
